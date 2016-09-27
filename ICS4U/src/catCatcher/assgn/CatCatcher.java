f843b9b54c327c1b518ed69294c6e10e465240c2
Maybe it's Jordan <susanbrodt@Susans-MacBook-Pro.local>
2016-09-27 19:45:27 +0200

    Revert "Eyyy Cat Catcher evolves"
    This reverts commit 54aa6dbb60f48ac1f9ef1069112f086f5c813149.

--- ICS4U/src/catCatcher/assgn/CatCatcher.java
+++ ICS4U/src/catCatcher/assgn/CatCatcher.java
@@ -1,21 +1,15 @@
 package catCatcher.assgn;
 
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.Random;
 
 import javax.swing.*;
 
-public class CatCatcher extends JFrame implements ActionListener, MouseListener, Runnable {
+public class CatCatcher extends JFrame implements ActionListener {
 	
 	private ImageIcon w;
-	private Timer wTime, gameTimer;
+	private Timer wTime;
 	private JLabel wf;
-    private Random random=new Random();
-    
 	
 	public CatCatcher()
 	{
@@ -25,19 +19,16 @@ public class CatCatcher extends JFrame implements ActionListener, MouseListener,
 			wf = new JLabel(w);
 			JPanel panel = new JPanel ();
 			panel.add(wf);
-	        
+			
 			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			this.setExtendedState(MAXIMIZED_BOTH);
 			this.setTitle("WoodieCatcher");
 			this.setVisible(true);
-			String score;
 			
 			this.setContentPane (panel);
 			wTime=new Timer(1500,this);
 	        wTime.start();
-	        gameTimer=new Timer(10000,this); 
 			
-	        wf.addMouseListener(this);
 		}
 		
 	}
@@ -48,51 +39,7 @@ public class CatCatcher extends JFrame implements ActionListener, MouseListener,
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-        if (e.getSource()==wTime) {
-        }
-            // Move the cat randomly, subtract width or height so it stays on the screen
-            wf.setLocation(random.nextInt(getWidth()-wf.getWidth()), random.nextInt(getHeight()-wf.getHeight()));
-
-	}
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		wTime=new Timer(1500,this);
-        wTime.start();
-        gameTimer=new Timer(10000,this); 
-		String score = null;
-		JOptionPane.showMessageDialog(null,"Your score is "+score);
-        System.exit(0);
-
-	}
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-        actionPerformed(new ActionEvent(wTime,0,""));
-        wTime.restart();
-        Toolkit.getDefaultToolkit().beep();
-        int score = +1;
-
-	}
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+			
 	}
 
 }
