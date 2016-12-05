8f5d337578793f1414e6dee6afad624876d0a129
EduGenerator <susanbrodt@Susans-MacBook-Pro.local>
2016-12-05 23:06:40 +0100

    ad

--- ICS4U/src/recursive/tree/Branch.java
+++ ICS4U/src/recursive/tree/Branch.java
@@ -1,5 +1,11 @@
 package recursive.tree;
 
+import java.awt.*;
+
 public class Branch {
+	public void branch(Graphics g, int x, int y, int length,
+			   int angle, int angleChange, int num){
+		
+	}
 
 }
